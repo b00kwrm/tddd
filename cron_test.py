b841cd57c8b280a30e7c0169fab3ec78285b6e81
dave <dave@harpo.marx>
2019-01-17 22:29:24 -0500

    add sleep and adjust the cron test time down a minute

--- cron_test.py
+++ cron_test.py
@@ -4,7 +4,7 @@ import os
 
 time = datetime.now()
 minutes = time.strftime("%M")
-plus_two_minutes = str(int(minutes) + 2)
+plus_one_minutes = str(int(minutes) + 1)
 hour = time.strftime("%H")
 
 def create_evil_script():
@@ -12,14 +12,14 @@ def create_evil_script():
         f.write('ls > /tmp/evil.tmp\n')
     os.chmod('/tmp/evil.sh', 0o755)
         
-def create_evil_cron(plus_two_minutes, hour):
+def create_evil_cron(plus_one_minutes, hour):
     with open('/tmp/persistevil', 'w') as f:
-        f.write('{} {} * * * /tmp/evil.sh\n'.format(plus_two_minutes, hour))
+        f.write('{} {} * * * /tmp/evil.sh\n'.format(plus_one_minutes, hour))
 
 def schedule_evil_cron():
     subprocess.run(['crontab', '/tmp/persistevil'])
     
 if __name__ == '__main__':
     create_evil_script()
-    create_evil_cron(plus_two_minutes, hour)
+    create_evil_cron(plus_one_minutes, hour)
     schedule_evil_cron()
