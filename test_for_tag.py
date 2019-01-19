4b117d1283ea19e4b05d38d20e548eda245525d9
dave <dave@harpo.marx>
2019-01-19 11:05:31 -0500

    build custom docker with tag file in it.

---
+++ test_for_tag.py
@@ -0,0 +1,16 @@
+import csv
+
+tagged_lines = []
+
+with open('cron.csv') as f:
+    reader = csv.DictReader(f)
+    lines = list(reader)
+
+
+for line in lines:
+    if line['notes'] == 'T1168_local_job_scheduling':
+        tagged_lines.append(line['notes'])
+
+assert 'T1168_local_job_scheduling' in tagged_lines
+
+        
