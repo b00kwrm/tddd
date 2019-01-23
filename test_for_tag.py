f872beda08ee9a66e66da661c485835ea9f1ae63
dave <dave@harpo.marx>
2019-01-22 21:36:25 -0500

    add ssh test

--- test_for_tag.py
+++ test_for_tag.py
@@ -10,7 +10,9 @@ with open('tddd-cron-tagged.csv') as f:
 for line in lines:
     if line['notes'] == 'T1168_local_job_scheduling':
         tagged_lines.append(line['notes'])
+    if line['notes'] == 'T1078_valid_accounts':
+        tagged_lines.append(line['notes'])
 
 assert 'T1168_local_job_scheduling' in tagged_lines
-
+assert 'T1078_valid_accounts' in tagged_lines
         
