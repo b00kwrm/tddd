cee373a3b11c19a5dbaeae9553f797c3cfd1b9d5
dave <dave@harpo.marx>
2019-02-02 12:03:02 -0500

    added tag in tag file, moved user create from Dockerfile to start-stuff script

--- docker/start-stuff.sh
+++ docker/start-stuff.sh
@@ -3,6 +3,7 @@
 /etc/init.d/ssh start
 /etc/init.d/cron restart
 python3 /tmp/cron_test.py
+adduser --disabled-password --gecos "" oracle && echo "oracle:ginger" | chpasswd
 apt-get update && apt-get upgrade -y
 apt-get install linux-headers-amd64 -y
 tail -f /dev/null
