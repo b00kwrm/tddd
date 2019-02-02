2f8ccfcfc9ecc57364a6c6524bd68c7c3e05fd02
dave <dave@harpo.marx>
2019-02-02 10:31:18 -0500

    add test for tag counts

--- tests/test_for_tag.py
+++ tests/test_for_tag.py
@@ -3,6 +3,7 @@ import sys
 
 tagged_lines = []
 tag_events = []
+tag_counts = {}
 
 with open(sys.argv[1]) as f:
     reader = f.read()
@@ -25,8 +26,23 @@ for tag_event in tag_events:
         if tag_event[1]["tag"]["labels"][0] == "T1156_bash_profile_and_bashrc":
             tagged_lines.append(tag_event[1]["tag"]["labels"][0])
 
+# test that the tags got created
 assert "T1168_local_job_scheduling" in tagged_lines
 assert "T1078_valid_accounts" in tagged_lines
 assert "T1215_Kernel_Modules_and_Extensions" in tagged_lines
 assert "T1057_Process_Discovery" in tagged_lines
 assert "T1156_bash_profile_and_bashrc" in tagged_lines
+
+tag_set = set(tagged_lines)
+
+for tag in tag_set:
+    c = tagged_lines.count(tag)
+    tag_counts[tag] = c
+
+
+# test that the tags counts match what we expect.
+assert tag_counts["T1057_Process_Discovery"] == 1
+assert tag_counts["T1215_Kernel_Modules_and_Extensions"] == 3
+assert tag_counts["T1156_bash_profile_and_bashrc"] == 3
+assert tag_counts["T1078_valid_accounts"] == 1
+assert tag_counts["T1168_local_job_scheduling"] == 1
