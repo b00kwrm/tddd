24ec1ac01d172326b27cf644c0c41ff4ac8c4ef0
dave <dave@harpo.marx>
2019-02-02 08:14:47 -0500

    messing with docker paths and converting tag_test to do json instead of csv

--- tests/test_for_tag.py
+++ tests/test_for_tag.py
@@ -6,22 +6,22 @@ with open('tddd-cron-tagged.json') as f:
     reader = f.read()
     jf = json.loads(reader)
 
-
 for event in plaso.keys():
     if plaso[event].get('tag'):
         tag_events.append((event, plaso[event]))
 
 for tag_event in tag_events:
-    if line['notes'] == 'T1168_local_job_scheduling':
-        tagged_lines.append(line['notes'])
-    if line['notes'] == 'T1078_valid_accounts':
-        tagged_lines.append(line['notes'])
-    if line['notes'] == 'T1215_Kernel_Modules_and_Extensions':
-        tagged_lines.append(line['notes'])
-    if line['notes'] == 'T1057_Process_Discovery':
-        tagged_lines.append(line['notes'])
-    if line['notes'] == 'T1156_bash_profile_and_bashrc':
-        tagged_lines.append(line['notes'])
+    if tag_event[1].get('tag'):
+        if tag_event[1]['label'] == 'T1168_local_job_scheduling':
+            tagged_lines.append(tag_event)
+        if tag_event[1]['label'] == 'T1078_valid_accounts':
+            tagged_lines.append(tag_event)
+        if tag_event[1]['label'] == 'T1215_Kernel_Modules_and_Extensions':
+            tagged_lines.append(tag_event)
+        if tag_event[1]['label'] == 'T1057_Process_Discovery':
+            tagged_lines.append(tag_event)
+        if tag_event[1]['label'] == 'T1156_bash_profile_and_bashrc':
+            tagged_lines.append(tag_event)
 
 assert 'T1168_local_job_scheduling' in tagged_lines
 assert 'T1078_valid_accounts' in tagged_lines
