5e21cac588957e104b92e2737448f2934e141371
dave <dave@harpo.marx>
2019-02-05 05:56:46 -0500

    move python setup in to shell script

--- tools/install_python.sh
+++ tools/install_python.sh
@@ -1,6 +1,11 @@
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzvf Python-3.7.2.tgz
 cd Python-3.7.2
 ./configure
-make
+sudo make
 sudo make install
+cd ~/
+/usr/local/bin/python3.7 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
