978a096b4a20a4d45a313c05593dfa16099867cc
dave <dave@harpo.marx>
2019-02-05 23:33:12 -0500

    install python 3.6 and 3.7, because "hey why not"

--- tools/install_python.sh
+++ tools/install_python.sh
@@ -1,6 +1,13 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install build-essential checkinstall libbz2-dev libc6-dev libgdbm-dev libncursesw5-dev libreadline-gplv2-dev libssl-dev libsqlite3-dev tk-dev openssl libtemplate-perl zlib1g-dev
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+tar xzvf Python-3.7.2.tgz
+cd Python-3.7.2
+./configure --enable-optimizations >> conf_script.txt
+sudo make
+sudo make altinstall
+cd ..
 wget https://www.python.org/ftp/python/3.6.6/Python-3.6.6.tgz
 tar xzvf Python-3.6.6.tgz
 cd Python-3.6.6
