6f5cc26ab08680f2888b31bf0bcb7182cd7a4d67
dave <dave@harpo.marx>
2019-02-06 05:48:52 -0500

    install openssl, but this time make sure the new openssl in in /usr/bin/openssl.

--- tools/install_python.sh
+++ tools/install_python.sh
@@ -1,18 +1,20 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install build-essential checkinstall libbz2-dev libc6-dev libgdbm-dev libncursesw5-dev libreadline-gplv2-dev libssl-dev libsqlite3-dev tk-dev openssl libtemplate-perl zlib1g-dev
+wget https://www.openssl.org/source/openssl-1.0.2q.tar.gz
+tar xzvf openssl-1.0.2q.tar.gz
+cd openssl-1.0.2q
+sudo ./config
+sudo make
+sudo make install
+mv /usr/bin/openssl /usr/bin/openssl_ORIG
+sudo ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+cd ..
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzvf Python-3.7.2.tgz
 cd Python-3.7.2
 ./configure --enable-optimizations >> conf_script.txt
 sudo make
-sudo make altinstall
-cd ..
-wget https://www.python.org/ftp/python/3.6.6/Python-3.6.6.tgz
-tar xzvf Python-3.6.6.tgz
-cd Python-3.6.6
-./configure --enable-optimizations >> conf_script.txt
-make
 sudo make altinstall
 cd ~/repo
 /usr/local/bin/python3.6 -m venv venv
