5424bde9eea6b6b5d6e5a38d8214eca3480e8edf
dave <dave@harpo.marx>
2019-02-07 22:45:51 -0500

    export vars

--- tools/setup_python.sh
+++ tools/setup_python.sh
@@ -1,3 +1,6 @@
+export LDFLAGS="-L/usr/local/lib/"
+export LD_LIBRARY_PATH="/usr/local/lib/"
+export CPPFLAGS="-I/usr/local/include -I/usr/local/include/openssl"
 sudo apt-get build-dep python3.5
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzvf Python-3.7.2.tgz
