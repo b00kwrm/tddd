0f0bd385374438cdab6a3708ee0db505cba8a093
dave <dave@harpo.marx>
2019-02-07 20:06:13 -0500

    with-openssl=/usr/local/ssl

--- tools/install_python.sh
+++ tools/install_python.sh
@@ -16,7 +16,7 @@ cd ..
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzvf Python-3.7.2.tgz
 cd Python-3.7.2
-./configure
+./configure --with-openssl=/usr/local/ssl
 sudo make
 sudo make install
 cd /home/circleci/repo
