43831bd9461dfd2ec5575bd2e686da03f399f661
dave <dave@harpo.marx>
2019-02-08 05:34:06 -0500

    will this ever work again?

--- tools/setup_python.sh
+++ tools/setup_python.sh
@@ -1,6 +1,6 @@
-export LDFLAGS="-L/usr/local/lib/"
-export LD_LIBRARY_PATH="/usr/local/lib/"
-export CPPFLAGS="-I/usr/local/include -I/usr/local/include/openssl"
+export LDFLAGS="-L/usr/local/ssl/lib"
+export LD_LIBRARY_PATH="/usr/local/ssl/"
+export CPPFLAGS="-I/usr/local/ssl/include -I/usr/local/ssl/include"
 sudo apt-get build-dep python3.5
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
 tar xzvf Python-3.7.2.tgz
