29edb508cf47c19872aa43a6b1558ad609205c69
dave <dave@harpo.marx>
2019-02-09 07:30:36 -0500

    added another function to plaso_stats.py

--- plaso_stats/plaso_stats.py
+++ plaso_stats/plaso_stats.py
@@ -64,13 +64,30 @@ def count_tags(tag_events):
     return tag_counts
 
 
+def get_tag_and_body(tagged_events):
+    """Parse tagged events and return a tuple of
+       tag and body"""
+    tag_events = []
+    for tag_event in tagged_events:
+        body = tag_event[1].get("body")
+        tb = {"body": body}
+        tb["tags"] = []
+        tb["display_name"] = tag_event[1].get("display_name")
+        for tag in tag_event[1]["tag"]["labels"]:
+            tb["tags"].append(tag)
+        tag_events.append(tb)
+    return tag_events
+
+
 if __name__ == "__main__":
     tags = get_tags_from_file(sys.argv[1])
     jf = open_psort_json(sys.argv[2])
     tagged_events = get_tagged_events(jf)
     tag_status = check_for_tag(tags, tagged_events)
     tag_counts = count_tags(tagged_events)
+    tag_body = get_tag_and_body(tagged_events)
     pprint(tags)
     pprint(tagged_events)
     pprint(tag_status)
     pprint(tag_counts)
+    pprint(tag_body)
