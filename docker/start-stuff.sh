711cdbb3fef9224f47f42d8514b69309bafe141c
dave <dave@harpo.marx>
2019-03-03 07:47:01 -0500

    create a file in dev shm

--- docker/start-stuff.sh
+++ docker/start-stuff.sh
@@ -2,13 +2,11 @@
 /etc/init.d/rsyslog start
 /etc/init.d/ssh start
 /etc/init.d/cron restart
+echo "hello, /dev/shm" > /dev/shm/hello_shm.txt
 python3 /tmp/cron_test.py
 adduser --disabled-password --gecos "" oracle && echo "oracle:ginger" | chpasswd
 addgroup test_group
 adduser oracle test_group
 apt-get update && apt-get upgrade -y
 apt-get install linux-headers-amd64 -y
-cat <<EOF > /dev/shm/hello.txt
-hello, /dev/shm
-EOF
 tail -f /dev/null
